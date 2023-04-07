83182db705d08bf7cee3339c4498dad2f84364ee
HARON <harom.murumba22@gmail.com>
2023-04-07 10:09:19 -0700

    correction of 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""
+Defines a class Rectangle
+"""
 
 
 class Rectangle:
-    """variables to store values of dimensions in a rectangle"""
+    """Representation of a rectangle"""
     def __init__(self, width=0, height=0):
+        """Initializes the rectangle"""
         self.height = height
         self.width = width
 
