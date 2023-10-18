8b2360efd80e3db670b5d8c78c689325c2dd9e32
29008922 <haron.murumba22@gmail.com>
2023-10-18 04:05:42 -0700

    databases

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,4 @@
- --a script that creates the database called hbtn_0c_0
- CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+--a script that creates the database called hbtn_0c_0
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
 
