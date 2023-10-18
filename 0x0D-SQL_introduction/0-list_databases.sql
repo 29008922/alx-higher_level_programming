13db730fd4d5777e19170d97165d3a3f909df803
29008922 <haron.murumba22@gmail.com>
2023-10-18 01:04:28 -0700

    creating a database

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,4 +1,4 @@
----a script that lists all databases of your MySQL server
---- and below is the sql command
+--a script that lists all databases of your MySQL server
+-- and below is the sql command
 
 SHOW DATABASES;
