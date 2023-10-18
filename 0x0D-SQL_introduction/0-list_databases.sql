eabc2422c38aae8a80cf3949c89f7477bfe210c3
29008922 <haron.murumba22@gmail.com>
2023-10-18 03:57:13 -0700

    data base

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,4 +1,3 @@
---a script that lists all databases of your MySQL server
--- and below is the sql command
-
-SHOW DATABASES;
+ --a script that lists all databases of your MySQL server
+ -- and below is the sql command
+ SHOW DATABASES;
