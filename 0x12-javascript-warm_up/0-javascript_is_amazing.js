b3f1c152c1d04639ae3a2ea865cae991bedf933b
29008922 <haron.murumba22@gmail.com>
2023-11-13 06:45:01 -0800

    javascript is amazing

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,4 @@
+//a script that print to the console javascript is amazing
+
+const myVar = "JavaScript is amazing";
+console.log('myVar');
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,2 @@
+This project is inteded to help me learn all concept required to 
+be able to code in javascript programming language
