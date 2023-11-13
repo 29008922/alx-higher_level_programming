5345495a9789499e6429a894c3091b4f98f87e0f
29008922 <haron.murumba22@gmail.com>
2023-11-13 08:15:42 -0800

    i love c

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const myNumber = Math.floor(Number(process.argv[2]));
+if (isNaN(myNumber)){
+console.log('missing number of iccurence');
+}else {
+ for (let i = 0; i < myNumber; i++) {
+  console.log('C is fun');
+ }
+}
