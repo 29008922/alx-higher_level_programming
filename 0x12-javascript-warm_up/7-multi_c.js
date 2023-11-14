adc808a6782c04ca5ba06082c9c3227b5e33c260
haron murumba <haron.murumba22@gmail.com>
2023-11-14 00:10:18 -0800

    missing number of occurence

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
+const x = process.argv[2];
 
-const myNumber = Math.floor(Number(process.argv[2]));
-if (isNaN(myNumber)){
-console.log('missing number of occurence');
-}else {
- for (let i = 0; i < myNumber; i++) {
-  console.log('C is fun');
- }
+if (!parseInt(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
