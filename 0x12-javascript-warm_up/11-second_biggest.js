67d9b01fe242605baf292f69f76561f26f79037f
haron murumba <haron.murumba22@gmail.com>
2023-11-14 00:55:30 -0800

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,5 +3,5 @@ const nums = process.argv.slice(2).map(x => Number(x));
 if (nums.length <= 1) {
   console.log(0);
 } else {
-  console.log(nums.sort().reverse()[1]);
+    console.log(nums.sort().reverse()[1]);
 }
