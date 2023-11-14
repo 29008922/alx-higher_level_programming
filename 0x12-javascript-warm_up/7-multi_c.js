52253f73b513f3911a8ed79a1c549c9b66e4741f
haron murumba <haron.murumba22@gmail.com>
2023-11-14 00:07:09 -0800

    missing number of occurence

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,7 @@
 
 const myNumber = Math.floor(Number(process.argv[2]));
 if (isNaN(myNumber)){
-console.log('missing number of iccurence');
+console.log('missing number of occurence');
 }else {
  for (let i = 0; i < myNumber; i++) {
   console.log('C is fun');
