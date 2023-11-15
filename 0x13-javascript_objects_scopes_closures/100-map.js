62a3deaadf8a0b447fa8bd72e7e14d8247c8fe47
haron murumba <haron.murumba22@gmail.com>
2023-11-15 07:16:19 -0800

    maps

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,27 +1,6 @@
 #!/usr/bin/node
-'use strict';
+const list = require('./100-data.js').list;
 
-const request = require('request');
-const movieId = process.argv[2];
-const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(`Error: ${error}`);
-    return;
-  }
-
-  const data = JSON.parse(body);
-
-  data.characters.forEach((url) => {
-    request(url, (error, response, body) => {
-      if (error) {
-        console.error(`Error: ${error}`);
-        return;
-      }
-
-      const character = JSON.parse(body);
-      console.log(character.name);
-    });
-  });
-});
+let newList = list.map((x, i) => x * i);
+console.log(list);
+console.log(newList)
