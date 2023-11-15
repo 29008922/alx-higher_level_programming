d3bfab4eb81c7966703c933731008d2cb18f7f9e
haron murumba <haron.murumba22@gmail.com>
2023-11-14 23:48:26 -0800

    task 11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,2 +1,27 @@
 #!/usr/bin/node
-exports.list = [1, 2, 3, 4, 5];
+'use strict';
+
+const request = require('request');
+const movieId = process.argv[2];
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(`Error: ${error}`);
+    return;
+  }
+
+  const data = JSON.parse(body);
+
+  data.characters.forEach((url) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        console.error(`Error: ${error}`);
+        return;
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  });
+});
