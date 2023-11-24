73f02ecfad80d5dc4a76cec14cfa571e64b0bbac
29008922 <haron.murumba22@gmail.com>
2023-11-24 08:10:37 -0800

    orm

--- object-mapping/7-model_state_fetch_all.py
+++ object-mapping/7-model_state_fetch_all.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This script adds the State object
-`Louisiana` to the database `hbtn_0e_6_usa`.
+This script lists all State objects
+from the database `hbtn_0e_6_usa`.
 """
 
 from sys import argv
@@ -10,7 +10,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    #Access to the database and get a state.
+    """
+    Access to the database and get the states
+    from the database.
+    """
 
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
@@ -19,8 +22,5 @@ if __name__ == "__main__":
 
     session = Session()
 
-    lou_state = State(name='Louisiana')
-    session.add(lou_state)
-    session.commit()
-    print('{0}'.format(lou_state.id))
-    session.close()
+    for instance in session.query(State).order_by(State.id):
+        print('{0}: {1}'.format(instance.id, instance.name))
--- object-mapping/8-model_state_fetch_first.py
+++ object-mapping/8-model_state_fetch_first.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-This script adds the State object
-`Louisiana` to the database `hbtn_0e_6_usa`.
+This script prints the first State object
+from the database `hbtn_0e_6_usa`.
 """
 
 from sys import argv
@@ -10,7 +10,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    #Access to the database and get a state.
+    """
+    Access to the database and get a state
+    from the database.
+    """
 
     db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3])
@@ -18,9 +21,9 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
 
     session = Session()
+    instance = session.query(State).order_by(State.id).first()
 
-    lou_state = State(name='Louisiana')
-    session.add(lou_state)
-    session.commit()
-    print('{0}'.format(lou_state.id))
-    session.close()
+    if instance is None:
+        print('Nothing')
+    else:
+        print('{0}: {1}'.format(instance.id, instance.name))
